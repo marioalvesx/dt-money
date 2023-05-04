4a65a25c4f473a2c60ea804dfae753c681130332
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-05-04 00:56:05 -0300

    Add React-Modal and create first modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void; 
+  // é uma função que nao recebe parametro e nao devolve nada.
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           New transaction
         </button>
+
+        
       </Content>
     </Container>
   )

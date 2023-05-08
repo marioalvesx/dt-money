ba4a7ad6757de4e609c3b40c19d1b98131b75eb9
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-05-07 23:49:57 -0300

    Update form in modal

--- src/components/NewTransacionalModal/index.tsx
+++ src/components/NewTransacionalModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import { Container } from "./styles";
 
 interface NewTransactionalModalProps {
   isOpen: boolean;
@@ -10,8 +12,27 @@ export function NewTransactionalModal({
   onRequestClose,
 }: NewTransactionalModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Add transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close Modal" />
+      </button>
+      <Container>
+        <h2>Add transaction</h2>
+
+        <input placeholder="Title" />
+        <input placeholder="Value" type="number" />
+        <input placeholder="Category" />
+        <button type="submit">Submit</button>
+      </Container>
     </Modal>
   );
 }

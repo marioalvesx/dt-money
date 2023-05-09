609bd33f6506d2edfaf7e55423f16e3629ac06f3
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-05-08 23:18:49 -0300

    Apply Darken and button types in modal

--- src/components/NewTransacionalModal/index.tsx
+++ src/components/NewTransacionalModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
+import outcomeImg from "../../assets/outcome.svg";
+import incomeImg from "../../assets/income.svg";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionalModalProps {
   isOpen: boolean;
@@ -30,6 +32,18 @@ export function NewTransactionalModal({
 
         <input placeholder="Title" />
         <input placeholder="Value" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Category" />
         <button type="submit">Submit</button>
       </Container>
